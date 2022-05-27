0880aa497e9bede414799a2a276716cbb2f50500
key <rlfbd5142@gmail.com>
2022-05-27 14:16:29 +0900

    [feature] 실제 테스트 작성해보기
    - 성공하는 테스트 작성하기
    - 실패하는 테스트 작성한 후 성공하게 만들기

--- src/test/java/junit1/ScoreCollectionTest.java
+++ src/test/java/junit1/ScoreCollectionTest.java
@@ -1,8 +1,10 @@
 package junit1;
 
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * - 테스트 대상 클래스 - 타겟 클래스
@@ -12,5 +14,34 @@ class ScoreCollectionTest {
 
     @Test
     public void test() {
+    }
+
+    @Test
+    public void answersArtihmeticMeanOfTwoNumbers() {
+        // given
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // when
+        int actualResult = collection.arithmeticMean();
+
+        // then
+        assertThat(actualResult).isEqualTo(6);
+    }
+
+    @Test
+    public void answersArithmeticMeanOfTwoNumbersFail() {
+        // given
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 4);
+
+        // when
+        int actualResult = collection.arithmeticMean();
+
+        // then
+        assertThat(actualResult).isNotEqualTo(5);
+        assertThat(actualResult).isEqualTo(4);
     }
 }
